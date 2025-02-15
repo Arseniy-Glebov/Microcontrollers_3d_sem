d45aad0e5d8fa96cff30d68591a4a8e562e6e652
Arseniy-Glebov <arseniy.glebov@gmail.com>
2025-02-15 18:00:45 +0300

    task 2 update

--- blink-usb/blink-usb.c
+++ blink-usb/blink-usb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
 
 #define PICO_DEFAULT_LED_PIN 25
@@ -11,18 +12,29 @@ int main()
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
     while (true) {
-        char marker = getchar();
-        printf("session\n");
-        // printf(marker);
-        // printf('\n');
-        if (marker == '1')
+        printf("Waiting for commands\n");
+        char cur_sym = getchar(); 
+        char command[50] = "";
+        int counter = 0;
+        while (cur_sym != '`')
         {
-            printf("on\n");
-            gpio_put(PICO_DEFAULT_LED_PIN, true);
-        }else if (marker == '0')
-        {
-            printf("off\n");
-            gpio_put(PICO_DEFAULT_LED_PIN, false);
+            command[counter] = cur_sym;
+            cur_sym = getchar();
+            counter++;
         }
+        printf(command);
+        printf("\n");
+        int time = (int)command;
+        
+        /*
+        time += 5;
+        time *= 10;
+        printf(time);
+        printf("\n");
+        
+        gpio_put(PICO_DEFAULT_LED_PIN, true);
+        sleep_ms(time);
+        gpio_put(PICO_DEFAULT_LED_PIN, false);
+        */
     }
 }
